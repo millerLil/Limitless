64b004d2ff57d054e35df71448fcdfeea401a97d
Deryn Brown <109038259+dbrown13@users.noreply.github.com>
2024-11-17 15:14:57 -0600

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -9,10 +9,10 @@ def home():
     <!DOCTYPE html>
 
      <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
-    <style>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Home Page</title>
+     <style>
       ul {
         list-style-type: none;
         margin: 0;
@@ -52,10 +52,10 @@ def home():
           color: black;
       }
     </style>
-  </head>
-  <body>
+    </head>
+    <body>
     <nav>
-      <ul>
+       <ul>
         <li><h2>Limitless</h2></li>
         <li><a href="#home">Home</a></li>
         <li><a href="#workout">Workout</a></li>
